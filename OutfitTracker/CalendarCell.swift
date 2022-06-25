8d60ec447afdb435685aba066a7fd1fadb3084df
Roger Newman <jatrainc@gmail.com>
2022-06-25 15:46:34 -0500

    first commit

---
+++ OutfitTracker/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  OutfitTracker
+//
+//  Created by Roger Newman on 1/17/22.
+//
+
+import UIKit
+
+class CalendarCell : UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
